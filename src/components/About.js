d0718afcb21e629d81417112e5b77fa21baa8d3e
OVECJOE <ovecjoe123@gmail.com>
2022-05-06 16:10:40 +0100

    Updated repo with project files

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+export default function About() {
+    return (
+        <div className="About__card">
+            <h3>About</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A nam
+                incidunt et saepe rerum placeat consequuntur distinctio harum
+                iusto ipsa!
+            </p>
+        </div>
+    )
+}
---
+++ src/components/Interests.js
@@ -0,0 +1,14 @@
+export default function Interests() {
+    return (
+        <div className="Interests__card">
+            <h3>Interests</h3>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Accusamus excepturi, quibusdam eaque eos ipsum consequuntur nesciunt
+                asperiores, a aspernatur veritatis cumque nostrum nam ab, velit sint
+                mollitia porro. Inventore, nisi.
+
+            </p>
+        </div>
+    )
+}
